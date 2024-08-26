25d29a195e2fe2c7042fb88e13ebd3882db7a877
Simon Deutsch <simon.johannes.deutsch@upstream-mobility.at>
2024-08-26 09:09:51 +0200

    Initial Commit

---
+++ KS/KSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KSApp.swift
+//  KS
+//
+//  Created by Simon Deutsch on 26.08.24.
+//
+
+import SwiftUI
+
+@main
+struct KSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
